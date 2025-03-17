27b3da5d99d2b255e78c6b2983549b726b80c827
Wilson Duarte <wilsonduarte312@gmail.com>
2025-03-17 17:29:13 -0500

    nuevos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'screens/home_page.dart';
 import 'screens/products_page.dart';
 import 'screens/orders_page.dart';
 import 'screens/accounting_page.dart';
 
 void main() {
-  // Ensure Flutter is initialized before using any platform channels
+  // Ensure Flutter is initialized
   WidgetsFlutterBinding.ensureInitialized();
   
+  // Initialize FFI for SQLite
+  sqfliteFfiInit();
+  
+  // Set the database factory
+  databaseFactory = databaseFactoryFfi;
   runApp(const RestaurantApp());
-}
 
+}
 class RestaurantApp extends StatelessWidget {
   const RestaurantApp({super.key});
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,11 +7,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurante_app/main.dart';
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const RestaurantApp());
 
     // Verify that our counter starts at 0.
